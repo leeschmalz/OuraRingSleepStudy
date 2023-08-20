94145b38c9249684ec28306df0e2922f2e2df5d6
Lee Schmalz <leeschmalz@gmail.com>
2023-08-19 21:08:42 -0500

    include old manually recorded data

--- pull_alcohol_data.py
+++ pull_alcohol_data.py
@@ -1,4 +1,5 @@
 from exist_api_client import getExistAttribute
+import pandas as pd
 
 alcohol_data = getExistAttribute('alcoholic_drinks', count=9999)
 
@@ -6,7 +7,16 @@ rows = []
 for entry in alcohol_data:
     rows.append({'date': entry['date'], 'name': 'alcohol', 'value': entry['value']})
 
-# write to csv
-import pandas as pd
 df = pd.DataFrame(rows)
-df.to_csv('./data/alcohol_data.csv', index=False)
+
+# read old manually recorded data
+df_old = pd.read_csv('./data/old_manual_alcohol_data.csv')
+
+df['date'] = pd.to_datetime(df['date'])
+df_old['date'] = pd.to_datetime(df_old['date'], format='%m/%d/%y')  # based on the old format '4/17/22'
+
+# Combine the dataframes
+df_combined = pd.concat([df, df_old], ignore_index=True)
+df_combined = df_combined.sort_values(by='date', ascending=True).reset_index(drop=True)
+
+df_combined.to_csv('./data/alcohol_data.csv', index=False)
